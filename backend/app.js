112ca4c028536150c5225448a9a5dcb28ba363cf
ashkan <azarqam@gmail.com>
2020-06-23 21:15:22 -0400

    add validators functions

--- backend/app.js
+++ backend/app.js
@@ -10,15 +10,53 @@ const port = process.env.PORT;
 app.use("/", express.static("build")); // Needed for the HTML and JS files
 app.use("/", express.static("./public")); // Needed for local assets
 
+// Here we start our endpoints
 app.get("/calc", async (req, res) => {
-  let userLatitude = req.query.latitude;
-  let userLongitude = req.query.longitude;
-  let issWeather;
-  let userWeather;
-  let issBody;
-  let issLatitude;
-  let issLongitude;
+  try {
+    let userLatitude = req.query.latitude;
+    let userLongitude = req.query.longitude;
+    let issWeather;
+    let userWeather;
+    let issBody;
+    let issLatitude;
+    let issLongitude;
+    validateParameters({ userLatitude, userLongitude, res });
+    // Finding the ISS coordinates
+    issBody = await issCollector();
+    validateIssBody({ issBody, res });
+    issLatitude = issBody.iss_position.latitude;
+    issLongitude = issBody.iss_position.longitude;
+    // Finding the distance between the user and nearest place to ISS on earth
+    const distance = await distanceCalc(
+      userLatitude,
+      userLongitude,
+      issLatitude,
+      issLongitude,
+      "K"
+    );
+    // Finding the weather detials for the region below the ISS and user location
+    let response = await weatherStack(
+      userLatitude,
+      userLongitude,
+      issLatitude,
+      issLongitude
+    );
+    validateWeatherStack({ response, res });
+    issWeather = response.wsForIssBody;
+    userWeather = response.wsForUserBody;
+    // Calculating the temperature difference between the international space station and user
+    const tempDif =
+      issWeather.current.temperature - userWeather.current.temperature;
+    validateTempDif({ tempDif, res, distance, issWeather });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+// Here we are done with endpoint
 
+// We start our validators and responses
+const validateParameters = (params) => {
+  const { userLatitude, userLongitude, res } = params;
   if (!userLatitude || !userLongitude) {
     return res.send(
       JSON.stringify({
@@ -42,89 +80,74 @@ app.get("/calc", async (req, res) => {
       })
     );
   }
+  return;
+};
 
-  try {
-    // Finding the ISS coordinates
-    issBody = await issCollector();
-    if (issBody.message !== "success") {
-      return res.send(
-        JSON.stringify({
-          code: 502,
-          success: false,
-          message: "ISS info not found!",
-        })
-      );
-    }
-    issLatitude = issBody.iss_position.latitude;
-    issLongitude = issBody.iss_position.longitude;
-
-    // Finding the distance between the user and nearest place to ISS on earth
-    const distance = distanceCalc(
-      userLatitude,
-      userLongitude,
-      issLatitude,
-      issLongitude,
-      "K"
+const validateIssBody = (params) => {
+  const { issBody, res } = params;
+  if (issBody.message !== "success") {
+    return res.send(
+      JSON.stringify({
+        code: 502,
+        success: false,
+        message: "ISS info not found!",
+      })
     );
-
-    // Finding the weather detials for the region below the ISS and user location
-    let response = await weatherStack(
-      userLatitude,
-      userLongitude,
-      issLatitude,
-      issLongitude
-    );
-    if (
-      "success" in response.wsForIssBody ||
-      "success" in response.wsForUserBody ||
-      isNaN(response.wsForIssBody.current.temperature) ||
-      isNaN(response.wsForUserBody.current.temperature) ||
-      !response.wsForIssBody.location.name ||
-      !response.wsForUserBody.location.name
-    ) {
-      return res.send(
-        JSON.stringify({
-          code: 404,
-          success: false,
-          message: "No weather found for location!",
-        })
-      );
-    }
-    issWeather = response.wsForIssBody;
-    userWeather = response.wsForUserBody;
-
-    // Calculating the temperature difference between the international space station and user
-    const tempDif =
-      issWeather.current.temperature - userWeather.current.temperature;
-    if (tempDif) {
-      return res.send(
-        JSON.stringify({
-          code: 200,
-          success: true,
-          content: {
-            distance: distance + "KM",
-            tempDif: tempDif + "C",
-            location: {
-              observationTime: issWeather.current.observation_time,
-              name: issWeather.location.name,
-              country: issWeather.location.country,
-              temperature: issWeather.current.temperature,
-              weatherDescription: issWeather.current.weatherDescription,
-              windspeed: issWeather.current.windspeed,
-              humidity: issWeather.current.humidity,
-              feelsLike: issWeather.current.feelsLike,
-              visibility: issWeather.current.visibility,
-              uvIndex: issWeather.current.uvIndex,
-              icon: issWeather.current.icon,
-            },
-          },
-        })
-      );
-    }
-  } catch (err) {
-    throw new Error(err);
   }
-});
+  return;
+};
+
+const validateWeatherStack = (params) => {
+  const { response, res } = params;
+  if (
+    "success" in response.wsForIssBody ||
+    "success" in response.wsForUserBody ||
+    isNaN(response.wsForIssBody.current.temperature) ||
+    isNaN(response.wsForUserBody.current.temperature) ||
+    !response.wsForIssBody.location.name ||
+    !response.wsForUserBody.location.name
+  ) {
+    return res.send(
+      JSON.stringify({
+        code: 404,
+        success: false,
+        message: "No weather found for location!",
+      })
+    );
+  }
+  return;
+};
+
+const validateTempDif = (params) => {
+  const { tempDif, res, distance, issWeather } = params;
+  if (tempDif) {
+    return res.send(
+      JSON.stringify({
+        code: 200,
+        success: true,
+        content: {
+          distance: distance + "KM",
+          tempDif: tempDif + "C",
+          location: {
+            observationTime: issWeather.current.observation_time,
+            name: issWeather.location.name,
+            country: issWeather.location.country,
+            temperature: issWeather.current.temperature,
+            weatherDescription: issWeather.current.weatherDescription,
+            windspeed: issWeather.current.windspeed,
+            humidity: issWeather.current.humidity,
+            feelsLike: issWeather.current.feelsLike,
+            visibility: issWeather.current.visibility,
+            uvIndex: issWeather.current.uvIndex,
+            icon: issWeather.current.icon,
+          },
+        },
+      })
+    );
+  }
+  return;
+};
+// Here we are done with our validators and responses
 
 let listener;
 const start = () => {
